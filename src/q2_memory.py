6bc209f133c1b2c622fc2b6aa2efa9f05745226f
= <=>
2024-10-20 22:35:52 -0300

    Implementación de q2 time y memory

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,34 @@
-from typing import List, Tuple
+import pandas as pd
+import re
+from collections import defaultdict
 
-def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+def q2_memory(file_path: str):
+    # Se utilizan chunks para administrar mejor el espacio 
+    chunk_size=1000
+    # Se utilizan expresiones regulares para encontrar emojis
+    emoji_re = re.compile(
+        "["                               
+        "\U0001F600-\U0001F64F"  # Emojis
+        "\U0001F300-\U0001F5FF"  # Símbolos
+        "\U0001F680-\U0001F6FF"  # Transporte y símbolos
+        "\U0001F1E0-\U0001F1FF"  # Banderas
+        "\U00002702-\U000027B0"  # Otros símbolos
+        "\U000024C2-\U0001F251"  # Símbolos adicionales
+        "]", flags=re.UNICODE
+    )
+    
+    # Creación del defaultdict
+    emoji_counter = defaultdict(int)
+
+    # Se lee y procesa el archivo en chunks
+    with pd.read_json(file_path, lines=True, chunksize=chunk_size) as reader:
+        for chunk in reader:
+            chunk['emojis'] = chunk['content'].apply(lambda x: emoji_re.findall(x))
+            for emojis in chunk['emojis']:
+                for emoji in emojis:
+                    emoji_counter[emoji] += 1
+
+    # 10 emojis más comunes
+    sorted_emojis = sorted(emoji_counter.items(), key=lambda x: x[1], reverse=True)[:10]
+
+    return sorted_emojis
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,31 @@
-from typing import List, Tuple
+import pandas as pd
+import re
 
-def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+def q2_time(file_path: str):
+    # Cargar los datos
+    df = pd.read_json(file_path, lines=True)
+    
+    # Se utilizan expresiones regulares para encontrar emojis    
+    emoji_re = re.compile(
+        "["                               
+        "\U0001F600-\U0001F64F"  # Emojis
+        "\U0001F300-\U0001F5FF"  # Símbolos
+        "\U0001F680-\U0001F6FF"  # Transporte y símbolos
+        "\U0001F1E0-\U0001F1FF"  # Banderas
+        "\U00002702-\U000027B0"  # Otros símbolos
+        "\U000024C2-\U0001F251"  # Símbolos adicionales
+        "]", flags=re.UNICODE
+    )
+    
+    # Solo emojis
+    df['emojis'] = df['content'].apply(lambda x: emoji_re.findall(x))
+    
+    # Genera una sola lista de emojis
+    all_emojis = [emoji for sublist in df['emojis'] for emoji in sublist]
+    
+    # Cuenta los emojis más comunes
+    emoji_counts = pd.Series(all_emojis).value_counts().nlargest(10)
+
+    result = list(emoji_counts.items())
+
+    return result
